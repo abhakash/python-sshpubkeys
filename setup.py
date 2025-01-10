84cd090fd002ba962452bab9e09c5ebd9fe46e6f
Abhinav Akash <abhinav.a.akash@oracle.com>
2025-01-10 11:14:29 +0530

    update package name

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with codecs_open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='sshpubkeys',
+    name='py3sshpubkeys',
     version='3.4.0',
     description='SSH public key parser',
     long_description=long_description,
